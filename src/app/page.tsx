879904893a49dce5675625724262fac69398a9b6
Thomas Albuquerque <tealbthomas@gmail.com>
2023-06-30 19:53:46 -0300

    Pequeno refatoramento.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import countryEmoji from 'country-emoji';
 const countriesData = require('country-codes-list');
@@ -12,75 +10,36 @@ const countryCodeAndCalling = countriesData.customList(
   '+{countryCallingCode}'
 );
 
-interface CountryOption {
-  value: string;
-  label: any;
-  countryCode: string;
-}
-
-let options: CountryOption[];
-let array: {
-  id: number;
+let arrayC: {
   countryCode: string;
   countryCalling: string;
-}[] = [
-  {
-    id: -1,
-    countryCode: `AA`,
-    countryCalling: `+00`,
-  },
-];
-let countriesKeys: string[] = Object.keys(countryCodeAndCalling);
+}[] = [];
+let countriesCodes: string[] = Object.keys(countryCodeAndCalling);
 let countriesCalling: string[] = Object.values(countryCodeAndCalling);
-for (let i = 0; i < countriesKeys.length; i++) {
-  array.push({
-    id: i,
-    countryCode: countriesKeys[i],
+for (let i = 0; i < countriesCodes.length; i++) {
+  arrayC.push({
+    countryCode: countriesCodes[i],
     countryCalling: countriesCalling[i],
   });
 }
-const array2 = array.filter((a) => flag(a.countryCode) !== undefined);
-const flag211 = flag('SS');
-array2.sort((a, b) => {
+const filteredArrayC = arrayC.filter((a) => flag(a.countryCode) !== undefined);
+const sorteredArrayC = filteredArrayC.sort((a, b) => {
   const callingA = parseInt(a.countryCalling.replace('+', ''));
   const callingB = parseInt(b.countryCalling.replace('+', ''));
   return callingA - callingB;
 });
 
-options = array2.map((country) => ({
+const options = sorteredArrayC.map((country) => ({
   value: country.countryCalling,
-  label: (
-    <div>{`${flag(country.countryCode)} ${country.countryCalling} ${
-      country.countryCode
-    }`}</div>
-  ),
+  label: <div>{`${flag(country.countryCode)} ${country.countryCalling}`}</div>,
   countryCode: country.countryCode,
 }));
 
-const defaultInitialCountry = options.filter((o) => o.countryCode === `BR`);
+const defaultInitialCountry = options.filter((o) => o.countryCode === 'BR');
 
 export default function Home() {
-  const [selectedOptionCountryCode, setSelectedOptionCountryCode] =
-    useState('BR');
-  useEffect(() => {
-    console.log('array2');
-    console.log(array2);
-    console.log('flag211');
-    console.log(flag211);
-    console.log('countryCodeAndCalling');
-    console.log(countryCodeAndCalling);
-    console.log('flag(`US`)');
-    console.log(flag(`US`));
-
-    console.log('countriesData');
-    console.log(countriesData);
-    console.log('options');
-    console.log(options);
-  }, []);
   return (
     <main className="flex min-h-screen flex-col items-cen ter p-24 bg-darkGray">
-      {/* <div>{Object.keys(countries).join(', ')}</div> */}
-      <div>{flag(`US`)}</div>
       <Select
         options={options}
         className="country-selector"
